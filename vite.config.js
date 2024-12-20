bfe82c33e8e70021bc8a5d7ba79406de01db099f
PreFS <susannadontha@gmail.com>
2024-12-20 20:38:51 +0530

    Build and deploy second

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
   root: 'src',
   plugins: [react()],
   "server.hmr.overlay":"false"
 })
+
+
 // import { defineConfig } from 'vite';
